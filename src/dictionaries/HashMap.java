2ba9f5f6150b5e756b3e92dca7f7f20a82ee11ee
TheProgramemer <stand3rs0n@gmail.com>
2021-07-08 15:24:45 -0700

    Finished How Its Made HashMap

--- src/dictionaries/HashMap.java
+++ src/dictionaries/HashMap.java
@@ -4,7 +4,7 @@ public class HashMap<K, V> implements Dictionary {
     private Entry[] entries;
     private int size;
     public HashMap() {
-        this.entries = new Entry[100000];
+        this.entries = new Entry[99999999];
         this.size = 0;
     }
 
@@ -20,31 +20,53 @@ public class HashMap<K, V> implements Dictionary {
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        long hashKey = HashingUtil.hash(key.toString());
+        if (entries[(int) hashKey] == null) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean containsValue(Object key) {
+        for (Entry<K,V> entry : this.entries) {
+            if (entry != null && entry.getValue().equals(key)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public void put(Object key, Object value) {
         this.size++;
+        long hashKey = HashingUtil.hash(key.toString());
+        entries[(int) hashKey] = new Entry(key, value);
     }
 
     @Override
     public void replace(Object key, Object value) {
-
+        if (this.containsKey(key)) {
+            long hashKey = HashingUtil.hash(key.toString());
+            entries[(int) hashKey] = new Entry(key, value);
+        }
     }
 
     @Override
     public Object get(Object key) {
+        if (this.containsKey(key)) {
+            long hashKey = HashingUtil.hash(key.toString());
+            return entries[(int) hashKey].getValue();
+        }
         return null;
     }
 
     @Override
     public void remove(Object key) {
-        this.size--;
+        if (this.containsKey(key)) {
+            long hashKey = HashingUtil.hash(key.toString());
+            entries[(int) hashKey] = null;
+            this.size--;
+        }
     }
 }
