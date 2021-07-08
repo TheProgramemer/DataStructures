10a2e6155bf51a33b2baf2558c916c090fd8b98d
TheProgramemer <stand3rs0n@gmail.com>
2021-07-08 14:05:01 -0700

    Finished How Its Made Queue code

--- src/lists/queues/Queue.java
+++ src/lists/queues/Queue.java
@@ -14,17 +14,44 @@ public class Queue<T> {
     }
 
     public boolean contains(T value) {
+        Node temp = this.head;
+        while (temp != null) {
+            if (((T) temp.value()).equals(value)) {
+                return true;
+            }
+            temp = temp.next();
+        }
         return false;
     }
 
     public T peek() {
-        return null;
+        if (this.head == null) {
+            return null;
+        }
+        return (T) this.head.value();
     }
 
     public T poll() {
-        return null;
+        if (this.head == null) {
+            return null;
+        }
+        T value = (T) this.head.value();
+        this.head = this.head.next();
+        if (this.head == null) {
+            this.tail = null;
+        }
+        return value;
     }
 
     public void push(T value) {
+        Node newNode = new Node(value, null);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+            return;
+        }
+
+        this.tail.setNext(newNode);
+        this.tail = newNode;
     }
 }
