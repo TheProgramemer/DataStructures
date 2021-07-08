6a52646e10555bd0784d3d8b8bd2b925ea417b9d
TheProgramemer <stand3rs0n@gmail.com>
2021-07-08 14:52:35 -0700

    Setup How Its Made HashMaps video

--- src/DataStructuresTest.java
+++ src/DataStructuresTest.java
@@ -1,8 +1,23 @@
-import dictionaries.HashingUtil;
+import dictionaries.Dictionary;
+import dictionaries.HashMap;
 
 public class DataStructuresTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("race hashes to: " + HashingUtil.hash("race"));
-        System.out.println("care hashes to: " + HashingUtil.hash("care"));
+        Dictionary<String, String> map = new HashMap();
+        map.put("eevee", "I love Eevee!");
+        System.out.println(map.containsKey("eevee"));
+        System.out.println(map.get("eevee"));
+        System.out.println(map.containsValue("I love Eevee!"));
+        System.out.println(map.containsValue("I love Ekans too!"));
+
+        map.put("ekans", "I love Ekans too!");
+        System.out.println(map.containsKey("ekans"));
+
+        map.replace("eevee", "I adore Eevee!");
+        System.out.println(map.get("eevee"));
+
+        map.remove("eevee");
+        System.out.println(map.containsKey("eevee"));
+        System.out.println(map.containsKey("ekans"));
     }
 }
