9281b10f1c9e7c19f41c6a25e1cc357215e26933
TheProgramemer <stand3rs0n@gmail.com>
2021-07-08 14:16:23 -0700

    Completed basic Hash Implementation

--- src/dictionaries/HashingUtil.java
+++ src/dictionaries/HashingUtil.java
@@ -2,7 +2,11 @@ package dictionaries;
 
 public class HashingUtil {
     public static long hash(String input) {
-        long hash = 0;
+        long hash = 7;
+        int hashPrime = 11;
+        for (char character : input.toCharArray()) {
+            hash = hash * hashPrime + (character - 'a');
+        }
         return hash;
     }
 }
